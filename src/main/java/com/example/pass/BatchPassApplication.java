216861bb3c1ba355454ec02f180d331fd60ce326
khanian <khanylee@gmail.com>
2023-12-17 11:56:56 +0900

    Initial commit

---
+++ src/main/java/com/example/pass/BatchPassApplication.java
@@ -0,0 +1,13 @@
+package com.example.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPassApplication.class, args);
+    }
+
+}
