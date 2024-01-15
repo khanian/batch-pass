aeb7bea4ff970ddf19d3841087865f2590a21304
khanian <khanylee@gmail.com>
2024-01-15 15:10:22 +0900

    add asyncItemProcessor, asyncItemWriter

---
+++ src/main/java/com/example/pass/job/pass/UsePassesJobConfiguration.java
@@ -0,0 +1,112 @@
+package com.example.pass.job.pass;
+
+import com.example.pass.repository.booking.BookingEntity;
+import com.example.pass.repository.booking.BookingRepository;
+import com.example.pass.repository.booking.BookingStatus;
+import com.example.pass.repository.pass.PassEntity;
+import com.example.pass.repository.pass.PassRepository;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.integration.async.AsyncItemProcessor;
+import org.springframework.batch.integration.async.AsyncItemWriter;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+import javax.persistence.EntityManagerFactory;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.Future;
+
+@Configuration
+public class UsePassesJobConfiguration {
+    private final int CHUNK_SIZE = 10;
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final EntityManagerFactory entityManagerFactory;
+    private final PassRepository passRepository;
+    private final BookingRepository bookingRepository;
+
+    public UsePassesJobConfiguration(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory, PassRepository passRepository, BookingRepository bookingRepository) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.entityManagerFactory = entityManagerFactory;
+        this.passRepository = passRepository;
+        this.bookingRepository = bookingRepository;
+    }
+
+    @Bean
+    public Job usePassesJob() {
+        return this.jobBuilderFactory.get("usePassesJob")
+                .start(usePassesStep())
+                .build();
+    }
+
+    @Bean
+    public Step usePassesStep() {
+        return this.stepBuilderFactory.get("usePassesStep")
+                .<BookingEntity, Future<BookingEntity>>chunk(CHUNK_SIZE)
+                .reader(usePassesItemReader())
+                .processor(usePassesAsyncItemProcessor())
+                .writer(usePassesAsyncItemWriter())
+                .build();
+    }
+
+    @Bean
+    public JpaCursorItemReader<BookingEntity> usePassesItemReader() {
+        return new JpaCursorItemReaderBuilder<BookingEntity>()
+                .name("usePassesItemReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("select b from BookingEntity b join fetch b.passEntity where b.status = :status and b.usedPass = false and b.endedAt < : endedAt")
+                .parameterValues(Map.of("status", BookingStatus.COMPLETED, "endedAt", LocalDateTime.now()))
+                .build();
+    }
+
+    @Bean
+    public AsyncItemProcessor<BookingEntity, BookingEntity> usePassesAsyncItemProcessor() {
+        AsyncItemProcessor<BookingEntity, BookingEntity> asyncItemProcessor = new AsyncItemProcessor<>();
+        asyncItemProcessor.setDelegate(usePassesItemProcessor());
+        asyncItemProcessor.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return asyncItemProcessor;
+    }
+
+    @Bean
+    public ItemProcessor<BookingEntity, BookingEntity> usePassesItemProcessor() {
+        return bookingEntity -> {
+            // 이용권 잔여 차감
+            PassEntity passEntity = bookingEntity.getPassEntity();
+            passEntity.setRemainingCount(passEntity.getRemainingCount() - 1);
+            bookingEntity.setPassEntity(passEntity);
+
+            // 이용권 사용여부 업데이트
+            bookingEntity.setUsedPass(true);
+            return bookingEntity;
+        };
+    }
+
+    @Bean
+    public AsyncItemWriter<BookingEntity> usePassesAsyncItemWriter() {
+        AsyncItemWriter<BookingEntity> asyncItemWriter = new AsyncItemWriter<>();
+        asyncItemWriter.setDelegate(usePassesItemWriter());
+        return asyncItemWriter;
+    }
+
+    @Bean
+    public ItemWriter<BookingEntity> usePassesItemWriter() {
+        return bookingEntities -> {
+            for (BookingEntity bookingEntity: bookingEntities) {
+                int updatedCount = passRepository.updateRemainingCount(bookingEntity.getPassSeq(), bookingEntity.getPassEntity().getRemainingCount());
+                if (updatedCount > 0) {
+                    bookingRepository.updateUsedPass(bookingEntity.getPassSeq(), bookingEntity.isUsedPass());
+                }
+            }
+        };
+    }
+}
